5d472d65d9b8d483862e2bc4131405eb771e7af9
Stojancho Jefremov <stojanco.jefremov@gmail.com>
2016-07-07 21:38:36 +0200

    Finished EntityDemo lesson

---
+++ EntityDemo/EntityDemo.WebApp/Controllers/AuthorsController.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityDemo.WebApp.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        // GET: Authors
+        public ActionResult Index()
+        {
+            var context = new SciFiContext();
+
+            
+            var authors = context.Authors;
+
+            foreach (var author in authors)
+            {
+                Console.WriteLine(author);
+            }
+
+            return View(authors);
+        }
+    }
+}
--- EntityDemo/MainConsole/Program.cs
+++ EntityDemo/MainConsole/Program.cs
@@ -14,28 +14,45 @@ namespace MainConsole
         {
             var context = new SciFiContext();
 
-            var author = new Author
-            {
-                Name = "Andy Weir",
-                BirthDate = new DateTime(1968, 7, 7)
-            };
-
-
-            Console.WriteLine(author);
-            context.Authors.Add(author);
-            context.SaveChanges();
-            Console.WriteLine(author);
-
-
-
-            //var authors = context.Authors;
-
-            //foreach (var author in authors)
+            //var author = new Author
             //{
-            //    Console.WriteLine(author);
-            //}
+            //    Name = "Andy Weir",
+            //    BirthDate = new DateTime(1968, 7, 7)
+            //};
+
+
+            //Console.WriteLine(author);
+            //context.Authors.Add(author);
+            //context.SaveChanges();
+            //Console.WriteLine(author);
+
+
+
+            var authors = context.Authors.Include("Novels");
+
+            foreach (var author in authors)
+            {
+                Console.WriteLine(author);
+            }
 
             //Console.WriteLine(authors.Count());
+
+            //var marsNovels = context.Novels.Where(n => n.Title.Contains("mars"));
+
+            //foreach (var novel in marsNovels)
+            //{
+            //    Console.WriteLine(novel);
+            //}
+
+
+            //var ksr = context.Authors.Single(a => a.ID == 274);
+            //ksr.Novels = context.Novels.Where(n => n.AuthorID == ksr.ID);
+
+            //Console.WriteLine(ksr);
+            //foreach (var novel in ksr.Novels)
+            //{
+            //    Console.WriteLine(novel);
+            //}
         }
     }
 }
